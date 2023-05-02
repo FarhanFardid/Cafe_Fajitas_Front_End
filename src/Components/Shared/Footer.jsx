c33b408604705be18495856e8fc90f5b105ed192
FarhanFardid <farhanfardid009@gmail.com>
2023-05-02 22:11:23 +0600

    footer added

--- src/Components/Shared/Footer.jsx
+++ src/Components/Shared/Footer.jsx
@@ -3,7 +3,31 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <h2>This is Footer</h2>
+           <footer className="bg-light">
+  <div className="container">
+    <div className="row">
+      <div className="col-md-4 col-sm-12 mb-3">
+        <h5>Cafe Fajitas</h5>
+        <p>123 Main St, Anytown USA</p>
+        <p>(123) 456-7890</p>
+      </div>
+      <div className="col-md-4 col-sm-6 mb-3">
+        <h5>Hours</h5>
+        <p>Mon-Fri: 9am-9pm</p>
+        <p>Sat-Sun: 10am-8pm</p>
+      </div>
+      <div className="col-md-4 col-sm-6 mb-3">
+        <h5>Connect With Us</h5>
+        <ul className="list-inline">
+          <li className="list-inline-item"><a href="#"><i className="fab fa-facebook"></i></a></li>
+          <li className="list-inline-item"><a href="#"><i className="fab fa-twitter"></i></a></li>
+          <li className="list-inline-item"><a href="#"><i className="fab fa-instagram"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
+
         </div>
     );
 };
