495890dd6e70a2b6e6ebbf69f0be59081f789fc5
FarhanFardid <farhanfardid009@gmail.com>
2023-05-02 23:00:07 +0600

    footer completed

--- src/Components/Shared/Footer.jsx
+++ src/Components/Shared/Footer.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
 
+import { AiFillFacebook,AiFillTwitterCircle,AiFillInstagram } from 'react-icons/ai';
+
 const Footer = () => {
     return (
         <div>
-           <footer className="bg-light">
+           <footer className="bg-dark">
   <div className="container">
-    <div className="row">
+    <div className="row p-2">
       <div className="col-md-4 col-sm-12 mb-3">
-        <h5>Cafe Fajitas</h5>
-        <p>123 Main St, Anytown USA</p>
-        <p>(123) 456-7890</p>
+        <h4 className='fw-bolder text-warning p-2'>Cafe Fajitas</h4>
+        <p className='text-info'>123 Main St, Monterrey, Mexico</p>
+        <p className='text-info'>(123) 456-7890</p>
       </div>
-      <div className="col-md-4 col-sm-6 mb-3">
-        <h5>Hours</h5>
-        <p>Mon-Fri: 9am-9pm</p>
-        <p>Sat-Sun: 10am-8pm</p>
+      <div className="col-md-4 col-sm-6 mb-3 p-2 ">
+        <h5  className='text-danger fw-bolder'>Hours</h5>
+        <p  className='text-info'>Mon-Fri: 9am-11pm</p>
+        <p  className='text-info'>Sat-Sun: 10am-11pm</p>
       </div>
-      <div className="col-md-4 col-sm-6 mb-3">
-        <h5>Connect With Us</h5>
+      <div className="col-md-4 col-sm-6 mb-3 text-primary p-2">
+        <h5 className='fw-bolder'>Connect With Us</h5>
         <ul className="list-inline">
-          <li className="list-inline-item"><a href="#"><i className="fab fa-facebook"></i></a></li>
-          <li className="list-inline-item"><a href="#"><i className="fab fa-twitter"></i></a></li>
-          <li className="list-inline-item"><a href="#"><i className="fab fa-instagram"></i></a></li>
+          <li className="list-inline-item"><a href="#">  <AiFillFacebook className='m-1 text-info' /></a></li>
+          <li className="list-inline-item"><a href="#"> <AiFillTwitterCircle className='m-1 text-info'/>
+</a></li>
+          <li className="list-inline-item"><a href="#"><AiFillInstagram className='m-1 text-info'/></a></li>
         </ul>
       </div>
     </div>
