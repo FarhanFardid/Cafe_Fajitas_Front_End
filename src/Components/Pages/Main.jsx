a93524f502625566ccc5e6d5d09bf1b64a1f994f
FarhanFardid <farhanfardid009@gmail.com>
2023-05-03 23:32:05 +0600

    toast added to recipe

--- src/Components/Pages/Main.jsx
+++ src/Components/Pages/Main.jsx
@@ -3,10 +3,13 @@ import React from 'react';
 import Footer from '../Shared/Footer';
 import Navigation from '../Shared/Navigation';
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Main = () => {
     return (
         <div>
+            <ToastContainer/>
             <Navigation></Navigation>
             <Outlet></Outlet>
             <Footer></Footer>
--- src/Components/Pages/Recipe.jsx
+++ src/Components/Pages/Recipe.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { HiHeart} from 'react-icons/hi';
+import { toast } from 'react-toastify';
 
 const Recipe = ({recipe}) => {
+
+   
     const {recipe_name,ingredients,cooking_method,ratings} = recipe;
+    const [dis,setDis] = useState(false)
+    const showToast = () =>{
+        toast.success(`${recipe_name} recipe is your favorite`);
+        setDis(!dis);
+    }
     return (
         <Card className='col-lg-4 '>
         
@@ -14,7 +22,7 @@ const Recipe = ({recipe}) => {
         <p><span className='fw-bold'>Cooking Method:</span><span className='text-dark'>{cooking_method}</span> </p>
         <p> <span className='fw-bold'>Ratings:</span> <span className='text-warning fw-bolder'>{ratings}</span></p>
           </Card.Text>
-          <div className=''> <Button className='my-2 bg-dark text-warning'>Favorite<HiHeart className='fs-4 m-1 text-danger'/></Button></div>
+          <div className=''> <Button onClick={showToast} className='my-2 bg-dark text-warning' disabled={dis} >Favorite<HiHeart className='fs-4 m-1 text-danger'/></Button></div>
         
         </Card.Body>
      
