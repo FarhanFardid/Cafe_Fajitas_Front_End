f67c74c9e185e1f5e410089a738b8a6c2bf6ba1c
FarhanFardid <farhanfardid009@gmail.com>
2023-09-14 17:14:37 +0600

    Register page updates

--- src/Components/Pages/Register.jsx
+++ src/Components/Pages/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import { useContext } from 'react';
 import { Button, Form, Row } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
